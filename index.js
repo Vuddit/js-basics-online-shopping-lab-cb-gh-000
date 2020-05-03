b99223eebc2104edf014857144f9a0b889afe96d
Rohit Kale <ruk22@cam.ac.uk>
2020-05-03 21:31:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  cart.push({
+    "itemName": item,
+    "itemPrice": Math.floor(Math.random() * 100) + 1
+  })
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var statement = "In your cart, you have"
+  var j = cart.length
+  if (j = 0) {
+    return "Your shopping cart is empty."
+  } else if (j = 1) {
+    return `${statement} ${cart[itemName[0]]} at $${cart[itemPrice[0]]}.`
+  } else {
+    for (let i = 0; i + 1 < j; i--) {
+      statement += ` ${cart[itemName[i]]} at $${cart[itemPrice[i]]},`
+    }
+    statement += ` and ${cart[itemName[j-1]]} at $${cart[itemPrice[j-1]]}.`
+    return statement
+  }
 }
 
 function total() {
