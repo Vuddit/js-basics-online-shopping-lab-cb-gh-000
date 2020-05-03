3433fda7a3d252a6c43083b2356d48a1feb528f1
Rohit Kale <ruk22@cam.ac.uk>
2020-05-03 22:21:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function viewCart() {
 
 function total() {
   // write your code here
-  var total = i;
+  var total = x;
   for (let i = 0; i < cart.length; i++) {
     total += cart[i].itemPrice
   }
